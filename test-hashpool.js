38a0c7eecb71dd7c4adade9dd31f091d487103ed
Josh Betz <j@joshbetz.com>
2022-11-19 11:31:27 -0600

    Exponentially backoff reconnecting down nodes
    Instead of connecting every 30 seconds by default, we use an exponential
    backoff function that developers can supply.

---
+++ test-hashpool.js
@@ -0,0 +1,23 @@
+const { HashPool } = require( './build' );
+
+async function sleep( ms ) {
+	return new Promise( resolve => setTimeout( resolve, ms ) );
+}
+
+async function main() {
+	const memcached = new HashPool( [ 'localhost:11211', 'localhost:11311' ] );
+	while ( true ) {
+		try {
+			const key = 'test';
+			console.log( memcached.hashring.get( key ) );
+			console.log( await memcached.set( key, 'value' ) );
+			console.log( await memcached.get( key ) );
+		} catch ( error ) {
+			console.log( error );
+		}
+
+		await sleep( 1000 );
+	}
+}
+
+main();
